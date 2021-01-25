862151b202403b0b0901f73e4472c9a5cb905d47
kim <kim.xie@datacube.hk>
2021-01-25 16:33:50 +0800

    fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,30 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  extends: ['plugin:vue/essential', '@vue/standard'],
+  globals: {
+    ai: true,
+    mc: true,
+    d3: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': 'off', // process.env.NODE_ENV === 'production' ? 'error' :
+    'no-debugger': 'off', // process.env.NODE_ENV === 'production' ? 'error' :
+    'prefer-const': 'off',
+    camelcase: 0,
+    eqeqeq: 2,
+    'no-dupe-args': 2,
+    semi: [2, 'always'],
+    'comma-dangle': 0,
+    'no-return-assign': 'off',
+    'no-prototype-builtins': 'off',
+    "no-useless-escape": 0,
+    quotes: [1, 'single']
+  },
+};
